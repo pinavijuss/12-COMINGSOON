5cfb87f3637af12128ccb6cbb561329c2591bad4
pinavijuss <pijus.dalgeda@gmail.com>
2021-06-16 20:43:30 +0300

    favicon

--- js/home.js
+++ js/home.js
@@ -1,10 +1,9 @@
-
 // IMPORT
 import { Clock } from "./components/clock/Clock.js";
 import { socials } from "./components/socials/socials.js";
+import { clockData } from "./data/clockData.js";
 import { socialsData } from "./data/socialsData.js";
 
 // EXECUTION
-new Clock('#clock_1');
-// new Clock(485545);
+new Clock('#clock_1', clockData);
 socials('footer .socials', socialsData);
